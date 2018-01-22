b329e9f2a3bcca808e3a7ad0b8c9fe8f5adead34
ktp693 <chang06151995@gmail.com>
2018-01-22 04:41:26 +0000

    creating model and validations

---
+++ app/models/article.rb
@@ -0,0 +1,7 @@
+class Article < ActiveRecord::Base
+  #need to add contraints
+  #makes sure the object does not have empty title before it hits the database
+  validates :title, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :description, presence:true, length: { minimum: 3, maximum: 200 }
+  
+end
