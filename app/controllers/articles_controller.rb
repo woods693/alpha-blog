6161c85df330f6c6b5b7868f3ab1ac3e343f789e
ktp693 <chang06151995@gmail.com>
2018-01-22 21:27:12 +0000

    adding the show feature, create fully working

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,8 +7,19 @@ class ArticlesController < ApplicationController
   def create
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    if @article.save
+      flash[:notice] = "The article successfully saved"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+      
+    #@article.save
+    #redirect_to articles_path(@article)
+  end
+  
+  def show
+    @article = Article.find(params[:id])
   end
   
   private
