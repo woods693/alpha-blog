9bbb663296128c03d27469a920884a84dc3a4316
ktp693 <chang06151995@gmail.com>
2018-01-24 07:14:28 +0000

    index page styling

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,6 @@ class Article < ActiveRecord::Base
   #need to add contraints
   #makes sure the object does not have empty title before it hits the database
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
-  validates :description, presence:true, length: { minimum: 3, maximum: 200 }
+  validates :description, presence:true, length: { minimum: 3, maximum: 5000 }
   
 end
