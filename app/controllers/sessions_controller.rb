693ffc32c3d8034e07e3db372e326c1fcba96c38
ktp693 <chang06151995@gmail.com>
2018-02-13 00:46:28 +0000

    logout feature

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,22 @@ class SessionsController < ApplicationController
   end
   
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #saving id into the session
+      session[:user_id] = user.id
+      flash.now[:success] = "Logged In"
+      redirect_to users_path(user)
+    else
+      flash.now[:danger] = "Incorrect Login Credentials"
+      render 'new'
+    end
   end
   
   def destroy
-  
+    session[:user_id] = nil
+    flash.now[:success] = "You have logged out"
+    redirect_to root_path
   end
   
   
